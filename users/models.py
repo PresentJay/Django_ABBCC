6c5e2b52b4970c907b4df08eda685681107b8892
PresentJay <presentj94@gmail.com>
2020-01-14 22:48:10 +0900

    user data-fields migrations

--- users/models.py
+++ users/models.py
@@ -3,8 +3,20 @@ from django.db import models
 
 # Create your models here.
 
-
 class User(AbstractUser):
-
-    pass
+    """ Custom User Model """
+    
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+    
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+    
+    avatar = models.ImageField(null=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True)
+    bio = models.TextField(default="")
 
