9937e232b8b030ecf830d8551097b2f2cf68c99a
PresentJay <presentj94@gmail.com>
2020-01-20 23:38:10 +0900

    #6.0 modifing room admin panel - list display, filter, search_bar

--- rooms/admin.py
+++ rooms/admin.py
@@ -6,23 +6,42 @@ from . import models
 
 @admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
-    
+
     """ Item Admin Definition """
-    
-    pass  
+
+    pass
+
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
-    
+
     """ Room Admin Definition """
 
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "address",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    list_filter = ("instant_book", "city", "country")
+
+    search_fields = ("=city", "^host__username")
     pass
+
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
-    
-    """ Photo Admin Definition """
-    
-    pass
 
+    """ Photo Admin Definition """
+
+    pass
 
