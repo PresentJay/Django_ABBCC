6011dea58bb55525cbfb9a41f34c8089b64c6247
PresentJay <presentj94@gmail.com>
2020-01-22 19:51:09 +0900

    #7 wrap room admin up

--- rooms/models.py
+++ rooms/models.py
@@ -2,70 +2,72 @@ from django.db import models
 from django_countries.fields import CountryField
 from core import models as core_models
 from users import models as user_models
+
 # Create your models here.
 
+
 class AbstractItem(core_models.TimeStampedModel):
-    
+
     """ abstract Item """
-    
+
     name = models.CharField(max_length=80)
-    #subtitle = models.CharField()
-    
+    # subtitle = models.CharField()
+
     class Meta:
-        abstract=True
-        
+        abstract = True
+
     def __str__(self):
         return self.name
 
 
 class RoomType(AbstractItem):
-    
+
     """ RoomType Model Definition """
-    
+
     class Meta:
         verbose_name = "Room Type"
 
 
 class Amenity(AbstractItem):
-    
+
     """ Amenity Model Definition """
-        
+
     class Meta:
         verbose_name_plural = "Amenities"
 
 
 class Facility(AbstractItem):
-    
+
     """ Facility Model Definition """
-    
+
     class Meta:
         verbose_name_plural = "Facilities"
-        
+
 
 class HouseRule(AbstractItem):
-    
+
     """ HouseRule Model Definition """
-    
+
     class Meta:
-        verbose_name = "House Rule"    
+        verbose_name = "House Rule"
 
 
 class Photo(core_models.TimeStampedModel):
-    
+
     """ Photo Model Definition """
-    
+
     caption = models.CharField(max_length=80)
     file = models.ImageField()
-    room = models.ForeignKey("Room", on_delete=models.CASCADE)
-    
+    room = models.ForeignKey("Room", related_name="photos", on_delete=models.CASCADE)
+
     def __str__(self):
         return self.caption
 
 
 class Room(core_models.TimeStampedModel):
-    
+
     """ Room Model Definition """
-    
+
     name = models.CharField(max_length=140)
     description = models.TextField()
     country = CountryField()
@@ -79,13 +81,15 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey("users.User", related_name="rooms", on_delete=models.CASCADE) 
-    #room points only one user (multi to one relation)
-    
-    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
-    amenities = models.ManyToManyField("Amenity", blank=True)
-    facilities = models.ManyToManyField("Facility", blank=True)
-    house_rules = models.ManyToManyField("HouseRule", blank=True)
-    
+    host = models.ForeignKey(
+        "users.User", related_name="rooms", on_delete=models.CASCADE
+    )
+    # room points only one user (multi to one relation)
+
+    room_type = models.ForeignKey("RoomType", related_name="rooms", on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField("Amenity", related_name="rooms", blank=True)
+    facilities = models.ManyToManyField("Facility", related_name="rooms", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", related_name="rooms", blank=True)
+
     def __str__(self):
         return self.name
