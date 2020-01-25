cc3f6d7a31d3572407258cc98027d66614c045cb
PresentJay <presentj94@gmail.com>
2020-01-25 15:57:08 +0900

    #8.5 photo admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from . import models
 
 # Register your models here.
@@ -102,5 +103,9 @@ class PhotoAdmin(admin.ModelAdmin):
 
     """ Photo Admin Definition """
 
-    pass
+    list_display = ("__str__", "get_thumbnail")
 
+    def get_thumbnail(self, obj):
+        return mark_safe(f'<img src="{obj.file.url}" />')
+
+    get_thumbnail.short_description = "Thumbnail"
