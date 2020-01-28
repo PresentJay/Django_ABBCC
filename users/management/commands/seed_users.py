59f7e53f2a647e7d6537ca58db9b47ab45712c4b
PresentJay <presentj94@gmail.com>
2020-01-28 21:33:36 +0900

    #9.2 django_seed - modifing faker's instance and seeding 52 users

--- users/management/commands/seed_users.py
+++ users/management/commands/seed_users.py
@@ -8,12 +8,17 @@ class Command(BaseCommand):
     help = "This command creates many users"
 
     def add_arguments(self, parser):
-        parser.add_argument("--number", help="How many users do you want to create?")
+        parser.add_argument(
+            "--number",
+            default=2,
+            type=int,
+            help="How many users do you want to create?",
+        )
 
     def handle(self, *args, **options):
-        number = options.get("number", 1)
+        number = options.get("number")
         seeder = Seed.seeder()
-        seeder.add_entity(User, number, {"is_staff": False, "is_superuser": False,})
+        seeder.add_entity(User, number, {"is_staff": False, "is_superuser": False})
         seeder.execute()
-        self.stdout.write(self.style.SUCCESS("Users created!"))
 
+        self.stdout.write(self.style.SUCCESS(f"{number} Users created!"))
