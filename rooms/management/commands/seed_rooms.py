af5d2d713c344906a6409ac742a9b80ca8448106
PresentJay <presentj94@gmail.com>
2020-01-30 23:47:26 +0900

    seed randomized amenities, facilities, houserules to each rooms

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -2,7 +2,6 @@ import random
 from django.core.management.base import BaseCommand
 from django.contrib.admin.utils import flatten
 from django_seed import Seed
-
 from rooms import models as room_models
 from users import models as user_models
 
@@ -43,14 +42,37 @@ class Command(BaseCommand):
 
         created_photos = seeder.execute()
         created_clean = flatten(list(created_photos.values()))
+        amenities = room_models.Amenity.objects.all()
+        facilities = room_models.Facility.objects.all()
+        rules = room_models.HouseRule.objects.all()
 
         for pk in created_clean:
+            # get each key of created_room
             room = room_models.Room.objects.get(pk=pk)
+
+            # add randomized photo
             for i in range(2, random.randint(3, 6)):
                 room_models.Photo.objects.create(
                     caption=seeder.faker.sentence(),
                     room=room,
                     file=f"room_photos/{random.randint(1,7)}.jpg",
                 )
+
+            # the methods of below are about "many to many field"
+
+            # add randomized amenities
+            for a in amenities:
+                if random.randint(0, 30) % 2 == 0:
+                    room.amenities.add(a)
+
+            # add randomized facilites
+            for f in facilities:
+                if random.randint(0, 2) % 2 == 0:
+                    room.facilities.add(f)
+
+            # add randomized house_rules
+            for r in rules:
+                if random.randint(0, 2) % 2 == 0:
+                    room.house_rules.add(r)
 
         self.stdout.write(self.style.SUCCESS(f"{number} rooms created!"))
