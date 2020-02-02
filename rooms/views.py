38a287cf47a5473c803fc4a41d14f6457828d8f0
PresentJay <presentj94@gmail.com>
2020-02-02 22:06:31 +0900

    using paginator and orphans setting

--- rooms/views.py
+++ rooms/views.py
@@ -1,24 +1,36 @@
 from math import ceil
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from . import models
 
 
 def all_rooms(request):
     page = request.GET.get("page", 1)
-    page = int(page or 1)
+
+    # manual paginator codes-
+    """ page = int(page or 1)
     page_size = 10
     limit = page_size * page
     offset = limit - page_size
     all_rooms = models.Room.objects.all()[offset:limit]
-    page_count = ceil(models.Room.objects.count() / page_size)
+    page_count = ceil(models.Room.objects.count() / page_size) """
+
+    # django paginator codes-
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10, orphans=5)
+    pages = paginator.get_page(page)
+
     return render(
         request,
         "rooms/home.html",
         context={
-            "rooms": all_rooms,
-            "page": page,
-            "page_count": page_count,
-            "page_range": range(1, page_count + 1),
+            ## in django paginator codes-
+            "pages": pages,
+            ## in manual paginator codes-
+            # "rooms": all_rooms,
+            # "page": page,
+            # "page_count": page_count,
+            # "page_range": range(1, page_count + 1),
         },
     )
 
