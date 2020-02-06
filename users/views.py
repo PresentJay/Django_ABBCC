3739ab289bab9f8e555c78152902fcc0c98a5ce5
PresentJay <presentj94@gmail.com>
2020-02-06 12:40:52 +0900

    create login forms and view

--- users/views.py
+++ users/views.py
@@ -1,15 +1,17 @@
 from django.views import View
 from django.shortcuts import render
+from . import forms
 
 # Create your views here.
 
 
 class LoginView(View):
     def get(self, request):
-        return render(request, "users/login.html", {})
+        form = forms.LoginForm()
+        return render(request, "users/login.html", {"form": form,})
 
-    def post(self, reqiest):
-        pass
+    def post(self, request):
+        form = forms.LoginForm(request.POST)
 
 
 """ as in FBV, it'll be like this below """
