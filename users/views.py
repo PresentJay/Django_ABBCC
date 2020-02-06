7c5188b4cf355708500b572895d2432da0328afc
PresentJay <presentj94@gmail.com>
2020-02-06 10:31:44 +0900

    start login functions - create urls

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
+from django.views import View
 from django.shortcuts import render
 
 # Create your views here.
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, "users/login.html", {})
+
+    def post(self, reqiest):
+        pass
+
+
+""" as in FBV, it'll be like this below """
+
+""" def login_view(request):
+    if request.method == "GET":
+        pass
+    elif request.method == 'POST':
+        pass """
