bd104eb280e35f2db25637607d0b712fbbd2bb72
PresentJay <presentj94@gmail.com>
2020-02-07 13:49:33 +0900

    complete login-out functions as hard way on django

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 # Create your views here.
@@ -14,11 +15,23 @@ class LoginView(View):
         form = forms.LoginForm(request.POST)
 
         if form.is_valid():
-            print(form.cleaned_data)
+            # print(form.cleaned_data)
             # cleaned_data is result of cleaning on methods.
             # if clean-methods have not return, it removes data on result of cleaning
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=email, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect(reverse("core:home"))
 
         return render(request, "users/login.html", {"form": form,})
+
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
 
 
 """ as in FBV, it'll be like this below """
