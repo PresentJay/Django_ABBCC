0cf4e4bd6bbbb9e38c71be8afdf23a513baa0fba
PresentJay <presentj94@gmail.com>
2020-02-14 20:42:17 +0900

    email verify - gmail - fin

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.mail import send_mail
 from django.utils.html import strip_tags
+from django.template.loader import render_to_string
 
 # Create your models here.
 
@@ -53,7 +54,9 @@ class User(AbstractUser):
         if self.email_confirmed is False:
             secret = uuid.uuid4().hex[:20]
             self.email_secret = secret
-            html_message = f"To verify yout account, click <a href='http://127.0.0.1:8000/users/verify/{secret}'>here<a>"
+            html_message = render_to_string(
+                "emails/verify_email.html", {"secret": secret}
+            )
             send_mail(
                 "Verify Airbnb Account",
                 strip_tags(html_message),
@@ -62,6 +65,7 @@ class User(AbstractUser):
                 fail_silently=False,
                 html_message=html_message,
             )
+            self.save()
         return
 
     def __str__(self):
--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse_lazy
 from django.contrib.auth import authenticate, login, logout
 from django.views.generic import FormView
 from . import forms
+from . import models
 
 # Create your views here.
 
@@ -86,3 +87,20 @@ class SignUpView(FormView):
         user.verify_email()
 
         return super().form_valid(form)
+
+
+def complete_verification(request, key):
+    try:
+        user = models.User.objects.get(email_secret=key)
+        user.email_confirmed = True
+        user.save()
+
+        # to do : add success message
+
+    except models.User.DoesNotExist:
+        # to do: add error
+
+        pass
+
+    return redirect(reverse("core:home"))
+
