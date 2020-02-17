e96353e0d1ffd6e45b51a3891c62536c85a04895
PresentJay <presentj94@gmail.com>
2020-02-17 19:40:09 +0900

    finishing login with github - handling exceptions

--- users/views.py
+++ users/views.py
@@ -126,17 +126,16 @@ def github_callback(request):
         if code is not None:
             client_id = os.environ.get("GITHUB_ID")
             client_secret = os.environ.get("GITHUB_SECRET")
-            request = requests.post(
+            token_request = requests.post(
                 f"https://github.com/login/oauth/access_token?client_id={client_id}&client_secret={client_secret}&code={code}",
                 headers={"Accept": "application/json"},
             )
-            result_json = request.json()
-            error = result_json.get("error", None)
+            token_json = token_request.json()
+            error = token_json.get("error", None)
             if error is not None:
                 raise GithubException()
             else:
-
-                access_token = result_json.get("access_token")
+                access_token = token_json.get("access_token")
                 api_request = requests.get(
                     "https://api.github.com/user",
                     headers={
@@ -144,18 +143,36 @@ def github_callback(request):
                         "Accept": "application/json",
                     },
                 )
-
                 profile_json = api_request.json()
                 username = profile_json.get("login", None)
                 if username is not None:
                     name = profile_json.get("name")
                     email = profile_json.get("email")
                     bio = profile_json.get("bio")
-                    user = models.User.objects.get(email=email)
+                    try:
+                        user = models.User.objects.get(email=email)
+                        if user.login_method != models.User.LOGIN_GITHUB:
+                            raise GithubException()
+                    except models.User.DoesNotExist:
+                        user = models.User.objects.create(
+                            email=email,
+                            first_name=name,
+                            username=email,
+                            bio=bio,
+                            login_method=models.User.LOGIN_GITHUB,
+                        )
+                        user.set_unusable_password()
+                        user.save()
+                    login(request, user)
+                    # request is parameter of github_callback method.
+                    # this is very important. request must be unchanged.
+                    # if once request is changed, you can't use login method
+                    return redirect(reverse("core:home"))
                 else:
                     return redirect(reverse("users:login"))
 
         else:
             raise GithubException()
     except GithubException:
+        # send error message
         return redirect(reverse("users:login"))
