954bf3e759818b1f18744d35e1c2b169f6892ac2
PresentJay <presentj94@gmail.com>
2020-02-25 00:56:35 +0900

    signup styling done / django form rendering usage

--- users/forms.py
+++ users/forms.py
@@ -5,8 +5,10 @@ from . import models
 
 class LoginForm(forms.Form):
 
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder": "Email"}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+    )
 
     # if you wanna validate some fields, the method's name should be starts with 'clean_'
     def clean(self):
@@ -25,12 +27,44 @@ class LoginForm(forms.Form):
 
 # using UserCreationForm
 class SignUpForm(UserCreationForm):
-
-    username = forms.EmailField(label="Email")
-
     class Meta:
         model = models.User
-        fields = ("email",)
+        fields = ("first_name", "last_name", "email")
+        widgets = {
+        "first_name": forms.TextInput(attrs={"placeholder": "First Name"}),
+        "last_name": forms.TextInput(attrs={"placeholder": "Last Name"}),
+        "email": forms.EmailInput(attrs={"placeholder": "Email Name"}),
+    }
+
+    
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+    )
+
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"})
+    )
+
+    def clean_password1(self):
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+
+        if password != password1:
+            raise forms.ValidationError("Password confirmation does not match")
+        else:
+            return password
+
+    def save(self, *args, **kwrgs):
+        user = super().save(commit=False)
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        # commit=False do not saves on django database immediately.
+        # because of making of username
+
+        user.username = email
+        user.set_password(password)
+        user.save()
 
 
 # before modelform - manual way
