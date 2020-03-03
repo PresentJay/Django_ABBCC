899e68ebecbf30ca4ceb063642e40b0e394786c6
PresentJay <presentj94@gmail.com>
2020-03-03 19:47:03 +0900

    room-detail : photo grid

--- rooms/models.py
+++ rooms/models.py
@@ -122,5 +122,7 @@ class Room(core_models.TimeStampedModel):
         (photo,) = self.photos.all()[:1]  # unpacking in python
         return photo.file.url
 
-    def get_next_four(self):
-        pass
+    def get_next_four_photos(self):
+        photos = self.photos.all()[1:5]
+        return photos
+
