3fc3fc7df682bd23a0c99174fe1a3a1cd386ad41
PresentJay <presentj94@gmail.com>
2020-03-04 11:29:12 +0900

    styling room detail :2(fin) + room review

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,19 @@ module.exports = {
     extend: {
       spacing: {
         "25vh": "25vh",
+        "40vh": "40vh",
         "50vh": "50vh",
+        "60vh": "60vh",
         "75vh": "75vh"
       },
       borderRadius: {
         "xl": '1.5rem'
       },
       minHeight: {
+        "30vh": "30vh",
+        "40vh": "40vh",
         "50vh": "50vh",
+        "60vh": "60vh",
         "75vh": "75vh"
       }
     },
