2e9a13740532a461c731fb4d4a40ffae64b92490
PresentJay <presentj94@gmail.com>
2020-03-05 17:11:51 +0900

    room-editor ownership security

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import ListView, DetailView, View, UpdateView
 from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
+from django.http import Http404
+from users import mixins as user_mixins
 from . import models, forms
 
 
@@ -103,7 +105,7 @@ class SearchView(View):
         return render(request, "rooms/search.html", {"form": form})
 
 
-class EditRoomView(UpdateView):
+class EditRoomView(user_mixins.LoggedInOnlyView, UpdateView):
     model = models.Room
     template_name = "rooms/room_edit.html"
     fields = (
@@ -125,3 +127,10 @@ class EditRoomView(UpdateView):
         "facilities",
         "house_rules",
     )
+
+    def get_object(self, queryset=None):
+        room = super().get_object(queryset=queryset)
+        if room.host.pk != self.request.user.pk:
+            raise Http404()
+        else:
+            return room
