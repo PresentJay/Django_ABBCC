0206efb14855fdb5fe41b74d61e9fd1984eb5498
PresentJay <presentj94@gmail.com>
2020-03-15 19:27:10 +0900

    calendar done

--- rooms/models.py
+++ rooms/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from django_countries.fields import CountryField
 from core import models as core_models
 from users import models as user_models
@@ -134,6 +135,12 @@ class Room(core_models.TimeStampedModel):
             return None
 
     def get_calendars(self):
-        this_month = Calendar(2019, 11)
-        next_month = Calendar(2019, 12)
-        return [this_month, next_month]
+        now = timezone.now()
+        this_year = now.year
+        this_month = now.month
+        next_month = this_month + 1
+        if this_month == 12:
+            next_month = 1
+        this_month_cal = Calendar(this_year, this_month)
+        next_month_cal = Calendar(this_year, next_month)
+        return [this_month_cal, next_month_cal]
